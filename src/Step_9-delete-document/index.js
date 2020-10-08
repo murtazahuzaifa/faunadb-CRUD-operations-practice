a40f761eabb5bc00cff214d4162faf2ae236bdd6
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2020-10-09 01:20:19 +0500

    step 7,8 and 9 added

---
+++ src/Step_9-delete-document/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const faunadb = require('faunadb');
+
+const q = faunadb.query; // creating a query variable
+
+(async () => {
+
+    if (process.env.FAUNADB_CHILD_DB_SERVER_SECRET) {
+
+        const client = new faunadb.Client({ secret: process.env.FAUNADB_CHILD_DB_SERVER_SECRET });
+
+        try {
+            const result = await client.query(
+                q.Delete(
+                    q.Ref(q.Collection('products'), "278819174238652935")
+            ));
+
+            console.log(result);
+        }
+        catch (error) {
+            if (error.request === 400 && error.message === 'instance already exists') {
+                console.log(error);
+            }
+            else {
+                console.log("UnKnown Error ==>>> ", error);
+            }
+        }
+
+    } else {
+        console.log('No FAUNADB_ADMIN_SECRET in .env file, kindly add one first');
+    }
+
+})()
